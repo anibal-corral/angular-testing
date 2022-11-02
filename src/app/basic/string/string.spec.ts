437f4dd669079cf0db24d508abd48febaa105f74
Anibal Corral <anibal.corral@gmail.com>
2022-11-02 15:40:46 -0400

    Testing string

--- src/app/basic/string/string.spec.ts
+++ src/app/basic/string/string.spec.ts
@@ -8,7 +8,14 @@ describe('Tests about strings', ()=>{
     it('Should return a string', ()=>{
         const resp = message('Anastasia');
         expect(typeof resp).toBe("string");
-    })
+    });
+
+    it('Should return a greeting with the parameter sent',()=>{
+        const name = 'Anastasia';
+        const resp = message(name);
+        expect(resp).toBe("Hi "+ name);
+        expect(resp).toContain(name);
+    });
 
 
 })
