056e7409ee970994cb897e9e1ea0204935596667
Anibal Corral <anibal.corral@gmail.com>
2022-11-02 15:53:21 -0400

    Testing arrays

---
+++ src/app/basic/arrays/arrays.spec.ts
@@ -0,0 +1,18 @@
+import { getCars } from './arrays';
+
+describe('Tesing arrays',()=>{
+    it('Should return array of 2 cars',()=>{
+    const resp = getCars();
+
+    // expect(resp.length).toBe(2);
+    expect(resp.length).toBeGreaterThanOrEqual(2);
+    });
+
+    it('Should exist Mercedez and Fiat',()=>{
+        const resp = getCars();
+    
+        // expect(resp.length).toBe(2);
+        expect(resp).toContain('Mercedez');
+        expect(resp).toContain('Fiat');
+        });
+})
