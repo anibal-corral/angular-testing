ee14df6fc8a0bcbb2bb5cbbb4f9fc34283e29a51
Anibal Corral <anibal.corral@gmail.com>
2022-11-07 10:37:39 -0500

    Testing observables"

--- src/app/advanced/doctor-router/doctor-router.component.spec.ts
+++ src/app/advanced/doctor-router/doctor-router.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DoctorRouterComponent } from './doctor-router.component';
 import { Router, ActivatedRoute } from '@angular/router';
-import { Observable, of } from 'rxjs';
+import { from, Observable, of, Subject } from 'rxjs';
 
 
 class FakeRouter{
@@ -11,7 +11,18 @@ class FakeRouter{
   }
 }
 class FakeActivatedRouter{
-  params:Observable<any> =of();
+  // params:Observable<any> =of();
+  private subject = new Subject();
+
+
+  push(value:any){
+    this.subject.next(value);
+
+  }
+
+  get params(){
+    return this.subject.asObservable();
+  }
 }
 
 describe('DoctorRouterComponent', () => {
@@ -25,7 +36,9 @@ describe('DoctorRouterComponent', () => {
         {provide:Router, useClass:FakeRouter},
         // Router, 
         // ActivatedRoute
-        {provide:ActivatedRoute, useClass:FakeActivatedRouter}
+        {provide:ActivatedRoute, useClass:FakeActivatedRouter, useValue:{
+          params:from([{id:'007'}])
+        }}
       ]
     })
     .compileComponents();
@@ -44,5 +57,14 @@ describe('DoctorRouterComponent', () => {
     const spy = spyOn(router,'navigate');
     component.saveDoctor();
     expect(spy).toHaveBeenCalledWith(['doctor','007'])
+  });
+
+  xit('Shold put id = new ',()=>{
+    component = fixture.componentInstance;
+
+    
+    expect(component.id).toBe('new');
+
+
   })
 });
